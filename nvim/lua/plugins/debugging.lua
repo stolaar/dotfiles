00640b56f74d2f68ed96f9afd5baea65c63fbfce
Aleksandar Stolikj <stolaaar@gmail.com>
2025-01-22 15:40:31 +0100

    feat: install nvim dap

---
+++ nvim/lua/plugins/debugging.lua
@@ -0,0 +1,59 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "leoluz/nvim-dap-go",
+    "rcarriga/nvim-dap-ui",
+    "tpope/vim-fugitive",
+    "folke/trouble.nvim",
+    "nvim-neotest/nvim-nio",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    require("dapui").setup()
+    require("dap-go").setup()
+
+    require("dap").adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        args = { "/home/astolic/Downloads/js-debug/src/dapDebugServer.js", "${port}" },
+      }
+    }
+
+    require("dap").configurations.typescript = {
+      {
+        type = "pwa-node",
+        request = "attach",
+        name = "Attach to service",
+        port = 9229,
+        cwd = "${workspaceFolder}",
+        smartStep = true,
+        restart = true,
+      },
+    }
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    vim.keymap.set("n", "<Leader>dp", function ()
+      dapui.toggle()
+    end, {})
+
+  end,
+}
